09b3d30cf6d605a9e64fd179ccddf4cd3bed4ee8
Oskar Lewandowski <oskar55514@wp.pl>
2020-05-30 21:47:06 +0200

    Dodanie nowej bazy danych

--- laravelapp.sql
+++ laravelapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 28 Maj 2020, 02:16
+-- Czas generowania: 30 Maj 2020, 21:46
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.4.3
 
@@ -25,12 +25,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `category_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Zrzut danych tabeli `categories`
+--
+
+INSERT INTO `categories` (`id`, `category_name`, `created_at`, `updated_at`) VALUES
+(1, 'Laptopy i komputery', '2020-05-29 15:20:50', '2020-05-29 15:20:50'),
+(2, 'Podzespoły komputerowe', '2020-05-29 15:20:50', '2020-05-29 15:20:50'),
+(3, 'Smartfony', '2020-05-29 15:20:50', '2020-05-29 15:20:50'),
+(4, 'Gaming', '2020-05-29 15:20:50', '2020-05-29 15:20:50'),
+(5, 'AGD', '2020-05-29 15:20:50', '2020-05-29 15:20:50'),
+(6, 'Akcesoria', '2020-05-29 15:20:50', '2020-05-29 15:20:50'),
+(7, 'Konsole i gry', '2020-05-29 15:20:50', '2020-05-29 15:20:50'),
+(8, 'Fotografia i wideo', '2020-05-29 15:20:50', '2020-05-29 15:20:50'),
+(9, 'TV i audio', '2020-05-29 15:20:50', '2020-05-29 15:20:50');
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `migrations`
 --
 
 CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -41,9 +69,11 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2020_05_27_140547_create_roles_table', 2),
-(4, '2020_05_27_140909_create_role_user_table', 2),
-(5, '2020_05_27_155156_create_products_table', 3);
+(3, '2020_05_27_140547_create_roles_table', 1),
+(4, '2020_05_27_140909_create_role_user_table', 1),
+(5, '2020_05_27_155156_create_products_table', 1),
+(6, '2020_05_28_153258_create_categories_table', 1),
+(7, '2020_05_28_154436_add_categories_to_products', 1);
 
 -- --------------------------------------------------------
 
@@ -52,8 +82,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -69,18 +99,18 @@ CREATE TABLE `products` (
   `price` decimal(10,2) NOT NULL,
   `description` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `category_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Zrzut danych tabeli `products`
 --
 
-INSERT INTO `products` (`id`, `product_name`, `price`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Samsung Galaxy S10', '3000.99', 'To jest najnowysz telefon od firmy Samsung w okazyjnej cenie.', '2020-05-27 14:34:26', '2020-05-27 20:01:42'),
-(2, 'Logitech G PRO', '487.99', 'To jest klawiatura firmy Logitech, jest super do grania w gry.', '2020-05-27 14:36:28', '2020-05-27 20:01:34'),
-(5, 'SteelSeries Rival 310', '129.87', 'To jest myszka firmy SteelSeries.', '2020-05-27 20:18:10', '2020-05-27 20:18:10'),
-(7, 'Logitech G Pro Mouse', '549.77', 'Myszka gamingowa od firmy Logitech.', '2020-05-27 22:11:01', '2020-05-27 22:11:01');
+INSERT INTO `products` (`id`, `product_name`, `price`, `description`, `created_at`, `updated_at`, `category_name`) VALUES
+(1, 'Samsung Galaxy s20', '3500.25', 'To jest nowy Samsung. Jest drogi ale fajny!!', '2020-05-29 15:21:39', '2020-05-30 17:08:18', 'Smartfony'),
+(7, 'Samsung Galaxy s10', '2800.23', 'To jest trochę starszy model Samsunga.', '2020-05-30 17:08:43', '2020-05-30 17:08:43', 'Smartfony'),
+(8, 'Sony PlayStation 4 Slim 500GB', '1359.00', 'Poznaj mniejszą i smuklejszą konsolę PlayStation 4 Slim. Poza nowym wyglądem urządzenie oferuje graczom niesamowite wrażenia wizualne dzięki obecności grafiki HDR.', '2020-05-30 17:09:48', '2020-05-30 17:09:48', 'Konsole i gry');
 
 -- --------------------------------------------------------
 
@@ -90,7 +120,7 @@ INSERT INTO `products` (`id`, `product_name`, `price`, `description`, `created_a
 
 CREATE TABLE `roles` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -100,8 +130,8 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '2020-05-27 12:23:23', '2020-05-27 12:23:23'),
-(2, 'user', '2020-05-27 12:23:23', '2020-05-27 12:23:23');
+(1, 'admin', '2020-05-29 15:20:04', '2020-05-29 15:20:04'),
+(2, 'user', '2020-05-29 15:20:04', '2020-05-29 15:20:04');
 
 -- --------------------------------------------------------
 
@@ -133,10 +163,10 @@ INSERT INTO `role_user` (`id`, `role_id`, `user_id`, `created_at`, `updated_at`)
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -147,12 +177,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@admin.pl', NULL, '$2y$10$ScJ92tTOxt8NRmWOKGy0KOwmOoOESQH9KktyqGMNBLoRfZ25Ukn86', NULL, '2020-05-27 12:23:23', '2020-05-27 12:23:23'),
-(2, 'user', 'user@user.pl', NULL, '$2y$10$.Nsh0uYxPu7lHb1FNjGsQO0A3nHtlCs5wdyvzHRpTGCF.uoZhmUna', NULL, '2020-05-27 12:23:23', '2020-05-27 12:23:23');
+(1, 'admin', 'admin@admin.pl', NULL, '$2y$10$ybxZbvDJd8OrNjXOb8Lbw.qgCxi4EWY4gpl.unz03KSS9dwBXyEsS', NULL, '2020-05-29 15:20:05', '2020-05-29 15:20:05'),
+(2, 'user', 'user@user.pl', NULL, '$2y$10$WQbrId4loSV2LyTYd3QI/eBaCUQYmGBkL/du214mP7aoLKFjv6KLC', NULL, '2020-05-29 15:20:05', '2020-05-29 15:20:05');
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeksy dla tabeli `migrations`
@@ -196,16 +232,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT dla tabeli `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT dla tabeli `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT dla tabeli `roles`
